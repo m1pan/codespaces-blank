8613f01fec37797648cece9d5aae41ae644a4c14
Michael <95124779+m1pan@users.noreply.github.com>
2023-10-24 23:27:17 +0000

    read in damped data

--- ME 2 Dynamics lab/analysis.py
+++ ME 2 Dynamics lab/analysis.py
@@ -1,3 +1,26 @@
 import numpy as np
 import csv as c
+from itertools import islice
 
+
+#read in damped data
+damped1 = []
+with open('X2sv00006.txt') as f:
+    for line in islice(f,10,None):
+        damped1.append((float(line.split(' ')[0]),float(line.split(' ')[-1])))
+
+damped2 = []
+with open('X2sv00007.txt') as f:
+    for line in islice(f,10,None):
+        damped2.append((float(line.split(' ')[0]),float(line.split(' ')[-1])))
+
+damped3 = []
+with open('X2sv00008.txt') as f:
+    for line in islice(f,10,None):
+        damped3.append((float(line.split(' ')[0]),float(line.split(' ')[-1])))
+
+damped4 = []
+with open('X2sv00009.txt') as f:
+    for line in islice(f,10,None):
+        damped4.append((float(line.split(' ')[0]),float(line.split(' ')[-1])))
+        
